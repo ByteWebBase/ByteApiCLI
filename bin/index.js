997c6fc67d8445f2d39ee6e7e98b7848ba52e840
vansin <vansin@aliyun.com>
2021-09-25 15:51:14 +0800

    feat: github oauth登录

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-
 import fs from "fs";
 import execa from "execa";
 import path from "path";
@@ -8,7 +7,7 @@ import createIndexTemplate from "./createIndexTemplate.js";
 import createPackageTemplate from "./createPackageTemplate.js";
 import questions from "./questions/index.js";
 import { createConfig } from "./config.js";
-
+import { fileURLToPath } from "url";
 import { Command } from "commander/esm.mjs";
 const program = new Command();
 
@@ -28,10 +27,19 @@ function mySum(value, total) {
 
 program
   .command("login")
-  .argument("<first>", "integer argument", myParseInt)
-  .argument("[second]", "integer argument", myParseInt, 1000)
-  .action((first, second) => {
-    console.log(`${first} + ${second} = ${first + second}`);
+  // .argument("<first>", "integer argument", myParseInt)
+  // .argument("[second]", "integer argument", myParseInt, 1000)
+  .action(() => {
+    // console.log('login')
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const koaPath = __dirname + "/oauth/github.js";
+    console.log(koaPath); // 当前文件所在的绝对路径。
+    execa("node", [koaPath], {
+      // cwd: getRootPath(),
+      stdio: [2, 2, 2],
+    });
+    // console.log(`${first} + ${second} = ${first + second}`);
   });
 
 program
@@ -43,38 +51,36 @@ program
 
 program
   .command("build")
-  .argument("<value...>", "values to be summed", mySum, 0)
-  .action((total) => {
-    console.log(`sum is ${total}`);
+  // .argument("<value...>", "values to be summed", mySum, 0)
+  .action(async () => {
+    const answer = await questions();
+
+    const config = createConfig(answer);
+
+    // // 1. 创建项目文件夹
+    console.log(chalk.blue(`创建项目文件夹:${config.packageName}`));
+    fs.mkdirSync(getRootPath());
+
+    // 2. 创建 index.js
+    console.log(chalk.blue(`创建 index.js`));
+    fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(config));
+    // 3. 创建 package.json
+    console.log(chalk.blue(`创建 package.json`));
+    fs.writeFileSync(
+      `${getRootPath()}/package.json`,
+      createPackageTemplate(config)
+    );
+
+    // 4. 安装依赖
+    console.log(chalk.blue(`安装依赖`));
+    execa("yarn", {
+      cwd: getRootPath(),
+      stdio: [2, 2, 2],
+    });
+
+    function getRootPath() {
+      return path.resolve(process.cwd(), config.packageName);
+    }
   });
 
 program.parse();
-
-// const answer = await questions();
-
-// const config = createConfig(answer);
-
-// // // 1. 创建项目文件夹
-// console.log(chalk.blue(`创建项目文件夹:${config.packageName}`));
-// fs.mkdirSync(getRootPath());
-
-// // 2. 创建 index.js
-// console.log(chalk.blue(`创建 index.js`));
-// fs.writeFileSync(`${getRootPath()}/index.js`, createIndexTemplate(config));
-// // 3. 创建 package.json
-// console.log(chalk.blue(`创建 package.json`));
-// fs.writeFileSync(
-//   `${getRootPath()}/package.json`,
-//   createPackageTemplate(config)
-// );
-
-// // 4. 安装依赖
-// console.log(chalk.blue(`安装依赖`));
-// execa("yarn", {
-//   cwd: getRootPath(),
-//   stdio: [2, 2, 2],
-// });
-
-// function getRootPath() {
-//   return path.resolve(process.cwd(), config.packageName);
-// }
---
+++ bin/oauth/github.js
@@ -0,0 +1,91 @@
+const Koa = require("koa");
+const router = require("koa-router")();
+const static = require("koa-static");
+const app = new Koa();
+const axios = require("axios-https-proxy-fix");
+// const axios = require('axios-https-proxy-fix')
+const querystring = require("querystring");
+const open = require("open");
+
+app.use(static(__dirname + "/"));
+const config = {
+  client_id: "0cd81b35aceac0e8db2a",
+  client_secret: "7850b66333944341012ba7e3dfae4c788cbd548d",
+};
+
+router.get("/auth/github/login", async (ctx) => {
+  var dataStr = new Date().valueOf();
+  //重定向到认证接口,并配置参数
+  var path = "https://github.com/login/oauth/authorize";
+  path += "?client_id=" + config.client_id;
+
+  //转发到授权服务器
+  ctx.redirect(path);
+});
+router.get("/auth/github/callback", async (ctx) => {
+  console.log("callback..", ctx.url);
+  const code = ctx.query.code;
+  const params = {
+    client_id: config.client_id,
+    client_secret: config.client_secret,
+    code: code,
+  };
+
+  // let res = await axios.post({
+  //     url: 'https://github.com/login/oauth/access_token',
+  //     proxy: true,
+  //     data: params
+  // })
+  console.log(process.env.NODE_ENV);
+
+  let res = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    params,
+    {
+      proxy: {
+        host: "127.0.0.1",
+        port: "7890",
+      },
+    }
+  );
+
+  console.log("login", res.data);
+  const access_token = querystring.parse(res.data).access_token;
+
+  // ctx.body = `登陆成功`
+
+  console.log("access_token", access_token);
+  res = await axios.get("https://api.github.com/user", {
+    headers: {
+      accept: "application/json",
+      Authorization: `token ${access_token}`,
+    },
+    proxy: {
+      host: "127.0.0.1",
+      port: "7890",
+    },
+  });
+
+  console.log("userAccess:", res.data);
+  ctx.body = `
+        <div align="center">
+        <h1>Hello ${res.data.login}, login success</h1>
+        <img src="${res.data.avatar_url}" alt=""/>
+        </div>
+    `;
+
+  setTimeout(() => {
+    console.log("log success");
+    process.exit(0);
+  }, 3000);
+});
+
+app.use(router.routes()); /*启动路由*/
+app.use(router.allowedMethods());
+app.listen(20219);
+
+const openBrowser = async () => {
+  await open("http://localhost:20219/auth/github/login");
+};
+
+openBrowser();
